5a1a5e1f70466f35b219e01cf4e6bdbd4fd10a60
davidhin <hello@davidhin.com>
2021-07-21 15:06:25 +0930

    Update dataset generation

--- sastvd/helpers/datasets.py
+++ sastvd/helpers/datasets.py
@@ -1,12 +1,47 @@
 import pandas as pd
 import sastvd as svd
 import sastvd.helpers.git as svdg
+from sklearn.model_selection import train_test_split
 
 
-def bigvul():
+def train_val_test_split_df(df, idcol, stratifycol):
+    """Add train/val/test column into dataframe."""
+    X = df[idcol]
+    y = df[stratifycol]
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=1
+    )
+    X_train, X_val, y_train, y_val = train_test_split(
+        X_train, y_train, test_size=0.25, random_state=1
+    )
+    X_train = set(X_train)
+    X_val = set(X_val)
+    X_test = set(X_test)
+
+    def path_to_label(path):
+        if path in X_train:
+            return "train"
+        if path in X_val:
+            return "val"
+        if path in X_test:
+            return "test"
+
+    df["label"] = df[idcol].apply(path_to_label)
+    return df
+
+
+def bigvul(minimal=False):
     """Read BigVul Data."""
+    savedir = svd.get_dir(svd.cache_dir() / "minimal_datasets")
+    if minimal:
+        try:
+            return pd.read_csv(savedir / "minimal_bigvul.csv")
+        except:
+            pass
     df = pd.read_csv(svd.external_dir() / "bigvul2020.csv.gzip", compression="gzip")
     df = df.rename(columns={"Unnamed: 0": "id"})
     df["dataset"] = "bigvul"
     svdg.mp_code2diff(df)
+    df = train_val_test_split_df(df, "id", "CWE ID")
+    df[["dataset", "id", "label"]].to_csv(savedir / "minimal_bigvul.csv", index=0)
     return df
