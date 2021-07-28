6cda4a7e8b3d79e9e3bcbcc2a4b837b6cebdfae6
davidhin <hello@davidhin.com>
2021-07-28 15:48:05 +0930

    Update test name

--- tests/003_sast_test.py
+++ tests/003_sast_test.py
@@ -2,7 +2,7 @@ import sastvd.helpers.datasets as svdd
 import sastvd.helpers.sast as sast
 
 
-def sast_test_1():
+def test_sast_1():
     df = svdd.bigvul()
     sample = df[df.id == 182193]
     ret = sast.run_sast(sample.before.item(), verbose=1)
