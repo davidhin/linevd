7f3138fdf3933e37f391eab93b697048b7006d9c
davidhin <hello@davidhin.com>
2021-07-29 10:46:24 +0930

    Modify generate graphs for preprocessing sast

--- sastvd/analysis/generate_graphs.py
+++ sastvd/analysis/generate_graphs.py
@@ -1,23 +1,28 @@
-from multiprocessing import Pool, cpu_count
+import pickle as pkl
 
+import numpy as np
 import sastvd as svd
 import sastvd.helpers.datasets as svdd
 import sastvd.helpers.joern as svdj
-from tqdm import tqdm
+import sastvd.helpers.sast as sast
+from pandarallel import pandarallel
+
+pandarallel.initialize(verbose=True, progress_bar=True)
 
 df = svdd.bigvul()
+df_splits = np.array_split(df, 100)
 
 
 def graph_helper(row):
     """Parallelise svdj functions."""
-    svdj.full_run_joern_from_string(row["func_before"], row["dataset"], row["id"])
+    savedir_before = svd.get_dir(svd.interim_dir() / row["dataset"] / "before")
+    savedir_after = svd.get_dir(svd.interim_dir() / row["dataset"] / "after")
+    # svdj.full_run_joern_from_string(row["before"], row["dataset"], row["id"])
+
+    sast_before = sast.run_sast(row["before"], verbose=1)
+    with open(savedir_before / f"{row['id']}.before.sast.pkl", "wb") as f:
+        pkl.dump(sast_before, f)
 
 
-items = df[["id", "dataset", "func_before"]].to_dict("records")
-with Pool(processes=cpu_count()) as pool:
-    for _ in tqdm(pool.imap_unordered(graph_helper, items), total=len(items)):
-        pass
-
-iid = 1
-path = svd.interim_dir() / f"{items[iid]['dataset']}/{items[iid]['id']}.c"
-svdj.get_node_edges(path)[0]
+for split in df_splits:
+    split.parallel_apply(graph_helper, axis=1)
