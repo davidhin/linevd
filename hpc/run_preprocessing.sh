7d41d2821cd6f43fd124662d74c2853c2a5c3e3f
davidhin <hello@davidhin.com>
2021-07-29 15:36:36 +0930

    Add preprocessing HPC script

---
+++ hpc/run_preprocessing.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH -p batch
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH --time=00:20:00
+#SBATCH --mem=4GB
+#SBATCH --array=1-1000
+#SBATCH --err="hpc/logs/prepros_%a.err"
+#SBATCH --output="hpc/logs/prepros_%a.out"
+#SBATCH --job-name="prepros"
+
+# Setup Python Environment
+module load Singularity
+
+# Start singularity instance
+singularity exec main.sif python sastvd/analysis/preprocessing.py $SLURM_ARRAY_TASK_ID
