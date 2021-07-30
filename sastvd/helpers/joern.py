9e5069d9ae4cf56c23a84602106d7834f8b86ef5
David Hin <hello@davidhin.com>
2021-07-30 16:56:28 +0930

    Add advanced graph traversal to Joern helpers

--- sastvd/helpers/joern.py
+++ sastvd/helpers/joern.py
@@ -1,9 +1,12 @@
 import json
 import random
+from collections import defaultdict
 from pathlib import Path
 
+import numpy as np
 import pandas as pd
 import sastvd as svd
+import scipy.sparse as sparse
 from graphviz import Digraph
 
 
@@ -98,6 +101,15 @@ def get_node_edges(filepath: str):
         nodes = nodes[
             ["id", "_label", "name", "code", "lineNumber", "controlStructureType"]
         ]
+
+    # Assign line number to local variables
+    with open(filepath, "r") as f:
+        code = f.readlines()
+    lmap = assign_line_num_to_local(nodes, edges, code)
+    nodes.lineNumber = nodes.apply(
+        lambda x: lmap[x.id] if x.id in lmap else x.lineNumber, axis=1
+    )
+    nodes = nodes.fillna("")
 
     # Assign node name to node code if code is null
     nodes.code = nodes.apply(lambda x: "" if x.code == "<empty>" else x.code, axis=1)
@@ -194,3 +206,99 @@ def full_run_joern_from_string(code: str, dataset: str, iid: str):
     with open(savepath, "w") as f:
         f.write(code)
     return full_run_joern(savepath)
+
+
+def neighbour_nodes(nodes, edges, nodeids: list, hop: int = 1, intermediate=True):
+    """Given nodes, edges, nodeid, return hop neighbours.
+
+    nodes = pd.DataFrame()
+
+    """
+    nodes_new = (
+        nodes.reset_index(drop=True).reset_index().rename(columns={"index": "adj"})
+    )
+    id2adj = pd.Series(nodes_new.adj.values, index=nodes_new.id).to_dict()
+    adj2id = {v: k for k, v in id2adj.items()}
+
+    arr = []
+    for e in zip(edges.innode.map(id2adj), edges.outnode.map(id2adj)):
+        arr.append([e[0], e[1]])
+        arr.append([e[1], e[0]])
+
+    arr = np.array(arr)
+    shape = tuple(arr.max(axis=0)[:2] + 1)
+    coo = sparse.coo_matrix((np.ones(len(arr)), (arr[:, 0], arr[:, 1])), shape=shape)
+
+    def nodeid_neighbours_from_csr(nodeid):
+        return [
+            adj2id[i]
+            for i in csr[
+                id2adj[nodeid],
+            ]
+            .toarray()[0]
+            .nonzero()[0]
+        ]
+
+    neighbours = defaultdict(list)
+    if intermediate:
+        for h in range(1, hop + 1):
+            csr = coo.tocsr()
+            csr **= h
+            for nodeid in nodeids:
+                neighbours[nodeid] += nodeid_neighbours_from_csr(nodeid)
+        return neighbours
+    else:
+        csr = coo.tocsr()
+        csr **= hop
+        for nodeid in nodeids:
+            neighbours[nodeid] += nodeid_neighbours_from_csr(nodeid)
+        return neighbours
+
+
+def rdg(edges, gtype):
+    """Reduce graph given type."""
+    if gtype == "reftype":
+        return edges[(edges.etype == "EVAL_TYPE") | (edges.etype == "REF")]
+    if gtype == "ast":
+        return edges[(edges.etype == "AST")]
+    if gtype == "pdg":
+        return edges[(edges.etype == "REACHING_DEF") | (edges.etype == "CDG")]
+
+
+def assign_line_num_to_local(nodes, edges, code):
+    """Assign line number to local variable in CPG."""
+    label_nodes = nodes[nodes._label == "LOCAL"].id.tolist()
+    onehop_labels = neighbour_nodes(nodes, rdg(edges, "ast"), label_nodes, 1, False)
+    twohop_labels = neighbour_nodes(nodes, rdg(edges, "reftype"), label_nodes, 2, False)
+    node_types = nodes[nodes._label == "TYPE"]
+    id2name = pd.Series(node_types.name.values, index=node_types.id).to_dict()
+    node_blocks = nodes[nodes._label == "BLOCK"]
+    blocknode2line = pd.Series(
+        node_blocks.lineNumber.values, index=node_blocks.id
+    ).to_dict()
+    local_vars = dict()
+    local_vars_block = dict()
+    for k, v in twohop_labels.items():
+        types = [i for i in v if i in id2name and i < 1000]
+        if len(types) == 0:
+            continue
+        assert len(types) == 1, "Incorrect Type Assumption."
+        block = onehop_labels[k]
+        assert len(block) == 1, "Incorrect block Assumption."
+        block = block[0]
+        local_vars[k] = id2name[types[0]]
+        local_vars_block[k] = blocknode2line[block]
+    nodes["local_type"] = nodes.id.map(local_vars)
+    nodes["local_block"] = nodes.id.map(local_vars_block)
+    local_line_map = dict()
+    for row in nodes.dropna().itertuples():
+        localstr = "".join((row.local_type + row.name).split()) + ";"
+        try:
+            ln = ["".join(i.split()) for i in code][int(row.local_block) :].index(
+                localstr
+            )
+            rel_ln = row.local_block + ln + 1
+            local_line_map[row.id] = rel_ln
+        except:
+            continue
+    return local_line_map
