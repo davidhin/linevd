8a259ab04220a050244de8738e37e13f687efd67
David Hin <hello@davidhin.com>
2021-08-02 14:21:06 +0930

    Complete 3 types of feature extraction

--- sastvd/ivdetect/main.py
+++ sastvd/ivdetect/main.py
@@ -1,5 +1,7 @@
 """Implementation of IVDetect."""
 
+import networkx as nx
+import pandas as pd
 import sastvd.helpers.joern as svdj
 
 
@@ -8,23 +10,44 @@ def feature_extraction(filepath, lineNumber: list = [], hop: int = 1):
     nodes, edges = svdj.get_node_edges(filepath)
 
     # 1. Generate tokenised subtoken sequences
-    f1_subseq = (
+    subseq = (
         nodes.sort_values(by="code", key=lambda x: x.str.len(), ascending=False)
         .groupby("lineNumber")
         .head(1)
     )
-    f1_subseq = f1_subseq[["lineNumber", "code", "local_type"]].copy()
-    f1_subseq.code = f1_subseq.apply(
+    subseq = subseq[["lineNumber", "code", "local_type"]].copy()
+    subseq.code = subseq.apply(
         lambda x: x.code + ";" if len(x.local_type) > 0 else x.code, axis=1
     )
-    f1_subseq.code = f1_subseq.local_type + " " + f1_subseq.code
-    f1_subseq = f1_subseq.drop(columns="local_type")
-    f1_subseq = f1_subseq[~f1_subseq.eq("").any(1)]
-    f1_subseq = f1_subseq[f1_subseq.code != " "]
-    f1_subseq.lineNumber = f1_subseq.lineNumber.astype(int)
-    f1_subseq = f1_subseq.sort_values("lineNumber")
+    subseq.code = subseq.local_type + " " + subseq.code
+    subseq = subseq.drop(columns="local_type")
+    subseq = subseq[~subseq.eq("").any(1)]
+    subseq = subseq[subseq.code != " "]
+    subseq.lineNumber = subseq.lineNumber.astype(int)
+    subseq = subseq.sort_values("lineNumber")
 
     # 2. Line to AST
+    ast_edges = svdj.rdg(edges, "ast")
+    ast_nodes = svdj.drop_lone_nodes(nodes, ast_edges)
+    ast = {"nodes": ast_nodes, "edges": ast_edges}
+
+    # 3. Variable names and types
+    reftype_edges = svdj.rdg(edges, "reftype")
+    reftype_nodes = svdj.drop_lone_nodes(nodes, reftype_edges)
+    reftype_nx = nx.Graph()
+    reftype_nx.add_edges_from(reftype_edges[["innode", "outnode"]].to_numpy())
+    reftype_cc = list(nx.connected_components(reftype_nx))
+    varnametypes = list()
+    for cc in reftype_cc:
+        cc_nodes = reftype_nodes[reftype_nodes.id.isin(cc)]
+        var_type = cc_nodes[cc_nodes["_label"] == "TYPE"].name.item()
+        for idrow in cc_nodes[cc_nodes["_label"] == "IDENTIFIER"].itertuples():
+            varnametypes += [[idrow.lineNumber, var_type, idrow.name]]
+    nametypes = pd.DataFrame(varnametypes, columns=["lineNumber", "type", "name"])
+    nametypes = nametypes.drop_duplicates().sort_values("lineNumber")
+
+    return subseq, ast, nametypes
+
     # BLAH BLAH BLAH
 
     # Group nodes into statements
@@ -59,15 +82,8 @@ def feature_extraction(filepath, lineNumber: list = [], hop: int = 1):
             (edges.innode.isin(keep_nodes)) & (edges.outnode.isin(keep_nodes))
         ]
 
-    # Drop lone nodes
-    nodes = nodes[(nodes.id.isin(edges.innode)) | (nodes.id.isin(edges.outnode))]
-
     # Visualise graph
-    dot = svdj.get_digraph(
-        nodes[["id", "node_label"]].to_numpy().tolist(),
-        edges[["outnode", "innode", "etype"]].to_numpy().tolist(),
-    )
-    dot.render("/tmp/tmp.gv", view=True)
+    svdj.plot_graph_node_edge_df(nodes, edges)
 
 
 filepath = "/home/david/Documents/projects/singularity-sastvd/storage/processed/bigvul/before/180189.c"
