af3794aaed404664bff2bf7bca460d1a8c3fa1c9
David Hin <hello@davidhin.com>
2021-08-03 07:40:59 +0930

    Add ivdetect tokenisation

--- sastvd/ivdetect/main.py
+++ sastvd/ivdetect/main.py
@@ -1,8 +1,29 @@
 """Implementation of IVDetect."""
 
+import re
+
 import networkx as nx
 import pandas as pd
 import sastvd.helpers.joern as svdj
+
+
+def tokenise(s):
+    """Tokenise according to IVDetect.
+
+    Tests:
+    s = "FooBar fooBar foo bar_blub23/x~y'z"
+    """
+    spec_char = re.compile(r"[^a-zA-Z0-9\s]")
+    camelcase = re.compile(r".+?(?:(?<=[a-z])(?=[A-Z])|(?<=[A-Z])(?=[A-Z][a-z])|$)")
+    spec_split = re.split(spec_char, s)
+    space_split = " ".join(spec_split).split()
+
+    def camel_case_split(identifier):
+        return [i.group(0) for i in re.finditer(camelcase, identifier)]
+
+    camel_split = [i for j in [camel_case_split(i) for i in space_split] for i in j]
+    remove_single = [i for i in camel_split if len(i) > 1]
+    return " ".join(remove_single)
 
 
 def feature_extraction(filepath):
@@ -40,6 +61,7 @@ def feature_extraction(filepath):
     subseq = subseq[subseq.code != " "]
     subseq.lineNumber = subseq.lineNumber.astype(int)
     subseq = subseq.sort_values("lineNumber")
+    subseq.code = subseq.code.apply(tokenise)
 
     # 2. Line to AST
     ast_edges = svdj.rdg(edges, "ast")[["innode", "outnode", "etype"]]
