797e293009cf7a80ff0a5e369dd637414a287b02
David Hin <hello@davidhin.com>
2021-08-03 11:10:58 +0930

    Extract out tokenisation function

---
+++ sastvd/helpers/tokenise.py
@@ -0,0 +1,20 @@
+import re
+
+
+def tokenise(s):
+    """Tokenise according to IVDetect.
+
+    Tests:
+    s = "FooBar fooBar foo bar_blub23/x~y'z"
+    """
+    spec_char = re.compile(r"[^a-zA-Z0-9\s]")
+    camelcase = re.compile(r".+?(?:(?<=[a-z])(?=[A-Z])|(?<=[A-Z])(?=[A-Z][a-z])|$)")
+    spec_split = re.split(spec_char, s)
+    space_split = " ".join(spec_split).split()
+
+    def camel_case_split(identifier):
+        return [i.group(0) for i in re.finditer(camelcase, identifier)]
+
+    camel_split = [i for j in [camel_case_split(i) for i in space_split] for i in j]
+    remove_single = [i for i in camel_split if len(i) > 1]
+    return " ".join(remove_single)
