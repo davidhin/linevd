f201542b4eda78ea06717faf073006783c2b9a44
David Hin <hello@davidhin.com>
2021-08-11 08:01:29 +0930

    Add graph helpers

---
+++ sastvd/helpers/graphs.py
@@ -0,0 +1,33 @@
+import matplotlib.pyplot as plt
+import networkx as nx
+from networkx.drawing.nx_pydot import graphviz_layout
+
+
+def simple_nx_plot(outnodes, innodes, node_labels):
+    """Plot graph for debugging purposes."""
+    import matplotlib as mpl
+
+    mpl.rcParams["figure.dpi"] = 300
+    labels = dict([(i, j) for i, j in enumerate(node_labels)])
+    G = nx.DiGraph(
+        list(zip(outnodes, innodes)),
+    )
+    G.add_nodes_from(range(len(node_labels)))
+
+    options = {
+        "font_size": 6,
+        "node_size": 300,
+        "node_color": "white",
+        "edgecolors": "black",
+        "linewidths": 0.5,
+        "width": 0.5,
+        "labels": labels,
+        "with_labels": True,
+    }
+    pos = graphviz_layout(G, prog="dot")
+    nx.draw_networkx(G, pos, **options)
+    # Set margins for the axes so that nodes aren't clipped
+    ax = plt.gca()
+    ax.margins(0.10)
+    plt.axis("off")
+    plt.show()
