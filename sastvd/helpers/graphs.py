ba864c6bb0375a44dad1c445a5c697a25f614574
David Hin <hello@davidhin.com>
2021-08-11 08:59:35 +0930

    Add dgl graph plot helper

--- sastvd/helpers/graphs.py
+++ sastvd/helpers/graphs.py
@@ -1,13 +1,13 @@
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 import networkx as nx
 from networkx.drawing.nx_pydot import graphviz_layout
 
+mpl.rcParams["figure.dpi"] = 300
+
 
 def simple_nx_plot(outnodes, innodes, node_labels):
     """Plot graph for debugging purposes."""
-    import matplotlib as mpl
-
-    mpl.rcParams["figure.dpi"] = 300
     labels = dict([(i, j) for i, j in enumerate(node_labels)])
     G = nx.DiGraph(
         list(zip(outnodes, innodes)),
@@ -23,6 +23,26 @@ def simple_nx_plot(outnodes, innodes, node_labels):
         "width": 0.5,
         "labels": labels,
         "with_labels": True,
+    }
+    pos = graphviz_layout(G, prog="dot")
+    nx.draw_networkx(G, pos, **options)
+    # Set margins for the axes so that nodes aren't clipped
+    ax = plt.gca()
+    ax.margins(0.10)
+    plt.axis("off")
+    plt.show()
+
+
+def simple_dgl_plot(dglgraph):
+    """Plot DGL graph simple."""
+    G = dglgraph.to_networkx()
+    options = {
+        "font_size": 6,
+        "node_size": 300,
+        "node_color": "white",
+        "edgecolors": "black",
+        "linewidths": 0.5,
+        "width": 0.5,
     }
     pos = graphviz_layout(G, prog="dot")
     nx.draw_networkx(G, pos, **options)
