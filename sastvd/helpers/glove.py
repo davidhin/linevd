ece4344d3802b2a755a11f2e495545fd81e61334
David Hin <hello@davidhin.com>
2021-08-10 10:38:47 +0930

    Add embeddings list helper

--- sastvd/helpers/glove.py
+++ sastvd/helpers/glove.py
@@ -125,3 +125,28 @@ def get_embeddings(text: str, emb_dict: dict, emb_size: int = 100) -> np.array:
     return [
         emb_dict[i] if i in emb_dict else np.full(emb_size, 0.001) for i in text.split()
     ]
+
+
+def get_embeddings_list(li: list, emb_dict: dict, emb_size: int = 100) -> list:
+    """Get embeddings from a list of sentences, then average.
+
+    Args:
+        li (list): List of sentences.
+        emb_dict (dict): Dict with key = token and val = embedding.
+        emb_size (int, optional): Size of embedding. Defaults to 100.
+
+    Example:
+    li = ['static long ec device ioctl xcmd struct cros ec dev ec void user arg',
+        'struct cros ec dev ec',
+        'void user arg',
+        'static long',
+        'struct cros ec dev ec',
+        'void user arg',
+        '']
+
+    glove_path = svd.processed_dir() / "bigvul/glove/vectors.txt"
+    emb_dict, _ = glove_dict(glove_path)
+    emb_size = 200
+    """
+    li = [i if len(i) > 0 else "<EMPTY>" for i in li]
+    return [np.mean(get_embeddings(i, emb_dict, emb_size), axis=0) for i in li]
