2222ca5ee50dce6e1ec47ab04a8d7d30bc600deb
David Hin <hello@davidhin.com>
2021-08-16 12:36:41 +0930

    update get func graph scala script

--- storage/external/get_func_graph.scala
+++ storage/external/get_func_graph.scala
@@ -3,5 +3,6 @@
    run.ossdataflow
    cpg.graph.E.map(node=>List(node.inNode.id, node.outNode.id, node.label, node.propertiesMap.get("VARIABLE"))).toJson |> filename + ".edges.json"
    cpg.graph.V.map(node=>node).toJson |> filename + ".nodes.json"
+   delete
 }
 
