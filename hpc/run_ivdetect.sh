5708c85abf0e682f0b13a8af84acaa3c4b6acc7c
David Hin <hello@davidhin.com>
2021-08-17 14:14:22 +0930

    Add run ivdetect script

---
+++ hpc/run_ivdetect.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH -p batch
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH --time=24:00:00
+#SBATCH --mem=16GB
+#SBATCH --gres=gpu:1
+#SBATCH --err="hpc/logs/ivdetect.info"
+#SBATCH --output="hpc/logs/ivdetect.info"
+#SBATCH --job-name="ivdetect"
+
+# Setup Python Environment
+module load Singularity
+module load CUDA/10.2.89
+
+# Start singularity instance
+singularity exec -H /g/acvt/a1720858/sastvd --nv main.sif python sastvd/ivdetect/main.py
