b656f8f5861952eed55287ced46506187504467d
David Hin <hello@davidhin.com>
2021-08-18 11:45:14 +0930

    Add plotting codebert

--- sastvd/helpers/codebert.py
+++ sastvd/helpers/codebert.py
@@ -1,5 +1,7 @@
+import matplotlib.pyplot as plt
 import torch
 from transformers import AutoModel, AutoTokenizer
+from tsne_torch import TorchTSNE as TSNE
 
 
 class CodeBert:
@@ -26,4 +28,26 @@ class CodeBert:
         tokens = [self.tokenizer.sep_token + " " + ct for ct in code_tokens]
         tk_args = {"padding": True, "truncation": True, "return_tensors": "pt"}
         tokens = self.tokenizer(tokens, **tk_args).to(self._dev)
-        return self.model(tokens["input_ids"])[1]
+        with torch.no_grad():
+            return self.model(tokens["input_ids"])[1]
+
+
+def plot_embeddings(embeddings, words):
+    """Plot embeddings.
+
+    import sastvd.helpers.datasets as svdd
+    cb = CodeBert()
+    df = svdd.bigvul()
+    sent = " ".join(df.sample(5).before.tolist()).split()
+    plot_embeddings(cb.encode(sent), sent)
+    """
+    tsne = TSNE(n_components=2, n_iter=2000, verbose=True)
+    Y = tsne.fit_transform(embeddings)
+    x_coords = Y[:, 0]
+    y_coords = Y[:, 1]
+    plt.scatter(x_coords, y_coords)
+    for label, x, y in zip(words, x_coords, y_coords):
+        plt.annotate(label, xy=(x, y), xytext=(0, 0), textcoords="offset points")
+    plt.xlim(x_coords.min() + 0.00005, x_coords.max() + 0.00005)
+    plt.ylim(y_coords.min() + 0.00005, y_coords.max() + 0.00005)
+    plt.show()
