4177a8bdda29a6fa981cfe66393d5c7405fa7785
davidhin <hello@davidhin.com>
2021-08-22 15:07:25 +0930

    Big update: BigVul filtering

--- sastvd/helpers/datasets.py
+++ sastvd/helpers/datasets.py
@@ -13,11 +13,15 @@ def train_val_test_split_df(df, idcol, stratifycol):
     """Add train/val/test column into dataframe."""
     X = df[idcol]
     y = df[stratifycol]
+    train_rat = 0.8
+    val_rat = 0.1
+    test_rat = 0.1
+
     X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.2, random_state=1
+        X, y, test_size=1 - train_rat, random_state=1
     )
-    X_train, X_val, y_train, y_val = train_test_split(
-        X_train, y_train, test_size=0.25, random_state=1
+    X_val, X_test, y_val, y_test = train_test_split(
+        X_test, y_test, test_size=test_rat / (test_rat + val_rat), random_state=1
     )
     X_train = set(X_train)
     X_val = set(X_val)
@@ -115,12 +119,46 @@ def bigvul(minimal=True, sample=False, return_raw=False):
     cols = ["func_before", "func_after", "id", "dataset"]
     svd.dfmp(df, svdg._c2dhelper, columns=cols, ordr=False, cs=300)
 
-    # Make splits
-    df = train_val_test_split_df(df, "id", "vul")
-
     # Assign info and save
     df["info"] = svd.dfmp(df, svdg.allfunc, cs=500)
     df = pd.concat([df, pd.json_normalize(df["info"])], axis=1)
+
+    # POST PROCESSING
+    dfv = df[df.vul == 1]
+    # No added or removed but vulnerable
+    dfv = dfv[~dfv.apply(lambda x: len(x.added) == 0 and len(x.removed) == 0, axis=1)]
+    # Remove functions with abnormal ending (no } or ;)
+    dfv = dfv[
+        ~dfv.apply(
+            lambda x: x.func_before.strip()[-1] != "}"
+            and x.func_before.strip()[-1] != ";",
+            axis=1,
+        )
+    ]
+    dfv = dfv[
+        ~dfv.apply(
+            lambda x: x.func_after.strip()[-1] != "}" and x.after.strip()[-1:] != ";",
+            axis=1,
+        )
+    ]
+    # Remove functions with abnormal ending (ending with ");")
+    dfv = dfv[~dfv.before.apply(lambda x: x[-2:] == ");")]
+
+    # Remove samples with mod_prop > 0.5
+    dfv["mod_prop"] = dfv.apply(
+        lambda x: len(x.added + x.removed) / len(x["diff"].splitlines()), axis=1
+    )
+    dfv = dfv.sort_values("mod_prop", ascending=0)
+    dfv = dfv[dfv.mod_prop < 0.7]
+    # Remove functions that are too short
+    dfv = dfv[dfv.apply(lambda x: len(x.before.splitlines()) > 5, axis=1)]
+    # Filter by post-processing filtering
+    keep_vuln = set(dfv.id.tolist())
+    df = df[(df.vul == 0) | (df.id.isin(keep_vuln))].copy()
+
+    # Make splits
+    df = train_val_test_split_df(df, "id", "vul")
+
     keepcols = [
         "dataset",
         "id",
