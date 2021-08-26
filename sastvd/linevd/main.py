b6eb5d709699eeba237b1a1a3ee3d1c9fac0d7d7
David Hin <hello@davidhin.com>
2021-08-26 12:09:05 +0930

    Extract running code

--- sastvd/linevd/main.py
+++ sastvd/linevd/main.py
@@ -1,5 +1,4 @@
 import os
-from glob import glob
 
 import dgl
 import pandas as pd
@@ -418,7 +417,7 @@ class LitGNN(pl.LightningModule):
         # Method level prediction from statement level
         method_level_pred = []
         method_level_true = []
-        for af in model.all_funcs:
+        for af in all_funcs:
             method_level_true.append(1 if sum(af[1]) > 0 else 0)
             pred_method = 0
             for logit in af[0]:
@@ -433,52 +432,3 @@ class LitGNN(pl.LightningModule):
     def configure_optimizers(self):
         """Configure optimizer."""
         return th.optim.AdamW(self.parameters(), lr=self.lr)
-
-
-# %%
-run_id = svd.get_run_id()
-samplesz = -1
-savepath = svd.get_dir(svd.processed_dir() / f"minibatch_tests_{samplesz}" / run_id)
-model = LitGNN(methodlevel=False, nsampling=True)
-
-# Load data
-data = BigVulDatasetLineVDDataModule(
-    batch_size=1024,
-    sample=samplesz,
-    methodlevel=False,
-    nsampling=True,
-    nsampling_hops=2,
-)
-
-# # Train model
-checkpoint_callback = pl.callbacks.ModelCheckpoint(monitor="val_loss")
-trainer = pl.Trainer(
-    gpus=1,
-    auto_lr_find=True,
-    default_root_dir=savepath,
-    num_sanity_val_steps=0,
-    callbacks=[checkpoint_callback],
-)
-tuned = trainer.tune(model, data)
-trainer.fit(model, data)
-
-# %% TESTING
-run_id = "202108251237_e14fc5f_codebert_only"
-run_id = "202108251105_5a6e846_update_train_code"
-best_model = glob(
-    str(
-        svd.processed_dir()
-        / f"minibatch_tests_{samplesz}"
-        / run_id
-        / "lightning_logs/version_0/checkpoints/*.ckpt"
-    )
-)[0]
-model = LitGNN.load_from_checkpoint(best_model, strict=False)
-trainer.test(model, data)
-
-model.res1vo
-model.res1
-model.res2
-model.res3
-model.res3vo
-model.res4
