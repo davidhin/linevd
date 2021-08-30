59b6c981471fc0e2248def8e63d3a270ddd9af71
David Hin <hello@davidhin.com>
2021-08-30 12:25:33 +0930

    Final update run script

--- sastvd/linevd/run.py
+++ sastvd/linevd/run.py
@@ -1,4 +1,5 @@
 from glob import glob
+from importlib import reload
 
 import pytorch_lightning as pl
 import sastvd as svd
@@ -8,7 +9,12 @@ run_id = svd.get_run_id()
 samplesz = -1
 savepath = svd.get_dir(svd.processed_dir() / f"minibatch_tests_{samplesz}" / run_id)
 model = lvd.LitGNN(
-    methodlevel=False, nsampling=True, model="gat2layer", loss="ce", hdropout=0.2
+    methodlevel=False,
+    nsampling=True,
+    model="gat2layer",
+    loss="ce",
+    hdropout=0.2,
+    multitask="linemethod",
 )
 
 # Load data
@@ -34,16 +40,13 @@ tuned = trainer.tune(model, data)
 trainer.fit(model, data)
 
 # %% TESTING
-# Uses old codebert embeddings (no attention mask)
-run_id = "202108251105_5a6e846_update_train_code"  # gat2layer
-run_id = "202108261224_1bd41cb_remove_print_line"  # mlp-only
-run_id = "202108261332_d1af738_update_codebert_encoding"  # gat2layer v2
-run_id = "202108261657_c42a446_load_everything_from_hparams"  # gat2layer 0.25 hdropout
-# Uses new codebert embeddings (has attention mask)
-run_id = "202108271123_8dd6708_update_joern_test_ids"  # mlp-only 0.2 hdropout
-run_id = "202108271328_8c84ea4_fix_torch_cat_func_embedding"  # mlponly+femb
+reload(lvd)
+run_id = "202108271123_8dd6708_update_joern_test_ids"  # Codebert single-task (line)
+run_id = "202108300848_107b176_whitespace_update"  # Codebert + GAT single-task (method)
+run_id = "202108301010_107b176_whitespace_update"  # Codebert + GAT single-task (line)
 
-run_id = "202108271622_8c84ea4_fix_torch_cat_func_embedding"
+run_id = "202108301139_8a89360_update_multitask_model"  # Codebert + GAT (multitask) + W
+run_id = "202108271658_2ac4767_update_default_codebert"  # Codebert + GAT (multitask)
 
 best_model = glob(
     str(
@@ -56,23 +59,12 @@ best_model = glob(
 model = lvd.LitGNN.load_from_checkpoint(best_model, strict=False)
 trainer.test(model, data)
 
+# %% Print results
 print(model.res1vo)
 print(model.res1)
 print(model.res2)
+print(model.res2mt)
+print(model.res2f)
 print(model.res3)
 print(model.res3vo)
 print(model.res4)
-
-# %% One run
-# from importlib import reload
-
-# reload(lvd)
-# model = lvd.LitGNN(methodlevel=False, nsampling=True, model="mlponly+femb")
-# # sample = next(iter(data.train_dataloader()))
-# model(sample)
-# # trainer.fit(model, data)
-
-# # %%
-# # Comments
-# # I really appreciate your presentation as I am always annoyed at not being able to run research code. Not a question, but just wanted to mention a few other things I've found useful. VSCode is actually quite powerful - I handle all source control, linting, and testing (pytest) all within the ide. For people who work HPC, sometimes conda environments are not sufficient, and Docker is unavailable due to security concerns. Singularity is a great alternative.
-# th.cat([th.Tensor([1, 2, 3]), th.Tensor([1, 2, 3])])
