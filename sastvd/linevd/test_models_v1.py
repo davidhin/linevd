099b37fb01f7a78d283f972911b7fd079b98691f
David Hin <hello@davidhin.com>
2021-09-06 15:19:49 +0930

    Modify test models script

--- sastvd/linevd/test_models_v1.py
+++ sastvd/linevd/test_models_v1.py
@@ -3,117 +3,26 @@ from importlib import reload
 
 import pytorch_lightning as pl
 import sastvd as svd
+import sastvd.helpers.dclass as svddc
+import sastvd.helpers.hljs as hljs
+import sastvd.helpers.rank_eval as svdhr
 import sastvd.linevd as lvd
+import torch as th
+from tqdm import tqdm
 
-# %% TESTING
-reload(lvd)
-result_dir = "minibatch_tests_-1"
-ckpt_str = "lightning_logs/version_0/checkpoints/*.ckpt"
-
-# Codebert single-task (line)
-run_id = "202108271123_8dd6708_update_joern_test_ids"
-
-# Codebert + GAT single-task (method)
-run_id = "202108300848_107b176_whitespace_update"
-
-# Codebert + GAT single-task (line)
-run_id = "202108301010_107b176_whitespace_update"
-
-# Codebert + GAT (multitask) Weighted 1:30
-run_id = "202108301139_8a89360_update_multitask_model"
-
-# Codebert + GAT (multitask) Weighted 1:1
-run_id = "202108271658_2ac4767_update_default_codebert"
-
-# Codebert + GAT (multitask) Weighted 1:5
-run_id = "202108301341_59b6c98_final_update_run_script"
-run_id = "202108311702_bfb2547_explicit_set_num_workers_getgraphs"
-
-# Codebert + GAT (multitask) Weighted 1:10
-run_id = "202108301341_59b6c98_final_update_run_script"
-
-# Codebert + GAT (multitask) Weighted 1:20
-run_id = "202108301536_40f42a0_update_run_layout"
-
-# Codebert (multitask) Weighted 1:20
-run_id = "202108310923_bfb2547_explicit_set_num_workers_getgraphs"
-
-# Codebert + GAT (multitask) Weighted 1:30
-run_id = "202108311203_bfb2547_explicit_set_num_workers_getgraphs"
-
-# Codebert + GAT8Head (multitask) Weighted 1:30
-run_id = "202108311552_bfb2547_explicit_set_num_workers_getgraphs"
-
-# Codebert + GAT (multitask) Weighted 1:5
-run_id = "202109011035_ea3f849_update_raytune_settings"
-result_dir = "raytune_-1"
-ckpt_str = "lightning_logs/version_2/checkpoints/*.ckpt"
-
-# Mystery (RayTune) Weighted 1:40
-run_id = "202109011703_74795f7_add_method_level_running"
-result_dir = "raytune_-1"
-ckpt_str = "lightning_logs/version_4/checkpoints/*.ckpt"
-
-# Mystery (RayTune) REPORTED IN PAPER (have to copy in first)
-run_id = "202109011703_74795f7_add_method_level_running"
-result_dir = "raytune_-1"
-ckpt_str = "lightning_logs/version_12/checkpoints/*.ckpt"
-
-# GCN (mystery)
-best_model = (
-    svd.processed_dir()
-    / "raytune_-1/202109011703_74795f7_add_method_level_running/tune_linevd/train_linevd_e8ecf_00015_15_gatdropout=0.2,gnntype=gcn,hdropout=0.25,hfeat=512,modeltype=gat1layer,stmtweight=1_2021-09-02_03-43-07/checkpoint_epoch=129-step=62270/checkpoint"
-)
-
-# GCN 2 layer - Best Res2MT and best Res2F, but low Res3VO and Res1VO
-best_model = (
-    svd.processed_dir()
-    / "raytune_-1/202109030833_a7bc051_add_method_level_testing/tune_linevd/train_linevd_090b7_00000_0_gatdropout=0.2,gnntype=gcn,hdropout=0.25,hfeat=512,modeltype=gat2layer,stmtweight=10_2021-09-03_08-33-50/checkpoint_epoch=66-step=32093/checkpoint"
-)
-
-# GCN 2 layer
-best_model = (
-    svd.processed_dir()
-    / "raytune_-1/202109030833_a7bc051_add_method_level_testing/tune_linevd/train_linevd_090b7_00001_1_gatdropout=0.15,gnntype=gcn,hdropout=0.25,hfeat=512,modeltype=gat2layer,stmtweight=15_2021-09-03_08-33-50/checkpoint_epoch=62-step=30177/checkpoint"
-)
-
-# GAT Raytune (same performance as in paper)
-best_model = (
-    svd.processed_dir()
-    / "raytune_-1/202109030833_a7bc051_add_method_level_testing/tune_linevd/train_linevd_090b7_00002_2_gatdropout=0.2,gnntype=gat,hdropout=0.25,hfeat=512,modeltype=gat1layer,stmtweight=15_2021-09-03_09-13-53/checkpoint_epoch=149-step=71850/checkpoint"
-)
-
-# GAT 1 layer PDG (could potentially be trained longer)
-best_model = (
-    svd.processed_dir()
-    / "raytune_-1/202109031120_0de0e64_update_raytune_hparam_gtype/tune_linevd/train_linevd_495df_00000_0_gatdropout=0.2,gnntype=gat,gtype=pdg,hdropout=0.25,hfeat=512,loss=ce,modeltype=gat1layer,scea=0.5,stmtw_2021-09-03_11-20-16/checkpoint_epoch=112-step=55031/checkpoint"
-)
-
-# GCN 1 layer cfgcdg + raw
-best_model = (
-    svd.processed_dir()
-    / "raytune_-1/202109031120_0de0e64_update_raytune_hparam_gtype/tune_linevd/train_linevd_495df_00001_1_gatdropout=0.15,gnntype=gcn,gtype=cfgcdg+raw,hdropout=0.3,hfeat=512,loss=ce,modeltype=gat1layer,scea=0._2021-09-03_11-20-16/checkpoint_epoch=98-step=47421/checkpoint"
-)
-
-
-best_model = (
-    svd.processed_dir()
-    / "raytune_-1/202109031120_0de0e64_update_raytune_hparam_gtype/tune_linevd/train_linevd_495df_00003_3_gatdropout=0.2,gnntype=gcn,gtype=pdg,hdropout=0.3,hfeat=512,loss=ce,modeltype=gat2layer,scea=0.5,stmtwe_2021-09-03_14-26-53/checkpoint_epoch=20-step=10227/checkpoint"
-)
+# PDG+RAW, Gat1Layer
+checkpoint = "raytune_-1/202109031655_f87dcf9_add_perfect_test/tune_linevd/train_linevd_2a3f5_00013_13_gatdropout=0.2,gnntype=gat,gtype=pdg+raw,hdropout=0.3,modeltype=gat2layer,stmtweight=10_2021-09-04_07-55-21/checkpoint_epoch=129-step=63310/checkpoint"
 
 # Load modules
-model = lvd.LitGNN(model="gat2layer", gnntype="gcn")
-data = lvd.BigVulDatasetLineVDDataModule(
-    batch_size=1024, nsampling_hops=2, gtype="cfgcdg"
-)
+model = lvd.LitGNN()
+datamodule_args = {"batch_size": 1024, "nsampling_hops": 2, "gtype": "pdg+raw"}
+data = lvd.BigVulDatasetLineVDDataModule(**datamodule_args)
 trainer = pl.Trainer(gpus=1, default_root_dir="/tmp/")
 
 # %% Load best model
-best_model = glob(str(svd.processed_dir() / result_dir / run_id / ckpt_str))[0]
+best_model = svd.processed_dir() / checkpoint
 model = lvd.LitGNN.load_from_checkpoint(best_model, strict=False)
 trainer.test(model, data)
-model.hparams
-
 
 # %% Print results
 print(model.res1vo)
@@ -127,8 +36,6 @@ print(model.res4)
 model.plot_pr_curve()
 
 # %% Find sample
-
-import torch as th
 
 
 def preds(model, data, vid):
@@ -145,11 +52,27 @@ def preds(model, data, vid):
     return ret
 
 
+def save_html_preds(vid, model, data):
+    """Save HTML visualised preds."""
+    line_preds = preds(model, data, vid)
+    vulns = [i[1] for i in line_preds if i[2] == 1]
+    line_preds = line_preds
+    line_preds = {i[1]: i[0] for i in line_preds}
+    hljs.linevd_to_html(svddc.BigVulDataset.itempath(vid), line_preds, vulns)
+
+
 # Finding suitable examples
 temp_df = data.test.df[data.test.df.vul == 1]
-temp_df = temp_df[(temp_df.before.str.len() > 700) & (temp_df.before.str.len() < 800)]
-
-sample = temp_df.iloc[4]
-preds(model, data, sample.id)
-
-print(sample.before)
+temp_df = temp_df[(temp_df.before.str.len() > 500) & (temp_df.before.str.len() < 800)]
+model.eval()
+reload(hljs)
+for i in tqdm(range(len(temp_df))):
+    sample = temp_df.iloc[i]
+    sorted_pred = [i[2] for i in preds(model, data, sample.id)]
+    try:
+        prec5 = svdhr.precision_at_k(sorted_pred, 5)
+        if prec5 > 0.7:
+            save_html_preds(sample.id, model, data)
+    except Exception as E:
+        print(E)
+        continue
