9d67104a17c95ab1685f709349123fffcfc5e0bf
David Hin <hello@davidhin.com>
2021-09-09 16:44:54 +0930

    Refactor scripts

--- sastvd/linevd/run.py
+++ sastvd/linevd/run.py
@@ -1,14 +1,14 @@
 import pytorch_lightning as pl
-import sastvd as svd
 import sastvd.linevd as lvd
-from ray import tune
 from ray.tune.integration.pytorch_lightning import (
     TuneReportCallback,
     TuneReportCheckpointCallback,
 )
 
 
-def train_linevd(config, samplesz=-1, max_epochs=130, num_gpus=1, checkpoint_dir=None):
+def train_linevd(
+    config, savepath, samplesz=-1, max_epochs=130, num_gpus=1, checkpoint_dir=None
+):
     """Wrap Pytorch Lightning to pass to RayTune."""
     model = lvd.LitGNN(
         hfeat=config["hfeat"],
@@ -52,46 +52,3 @@ def train_linevd(config, samplesz=-1, max_epochs=130, num_gpus=1, checkpoint_dir
     )
     trainer.tune(model, data)
     trainer.fit(model, data)
-
-
-# Hyperparameters
-config = {
-    "hfeat": tune.choice([512]),
-    "embtype": tune.choice(["codebert", "glove"]),
-    "stmtweight": tune.choice([1, 2, 5, 8, 10]),
-    "hdropout": tune.choice([0.25, 0.3]),
-    "gatdropout": tune.choice([0.15, 0.2]),
-    "modeltype": tune.choice(["gat1layer", "gat2layer", "mlponly"]),
-    "gnntype": tune.choice(["gat", "gcn"]),
-    "loss": tune.choice(["ce"]),
-    "scea": tune.choice([0.4, 0.5, 0.6]),
-    "gtype": tune.choice(["pdg", "pdg+raw"]),
-    "batch_size": tune.choice([1024]),
-    "multitask": tune.choice(["linemethod"]),
-    "splits": tune.choice(
-        [
-            "crossproject-linux",
-            "crossproject-Chrome",
-            "crossproject-Android",
-            "crossproject-qemu",
-        ]
-    ),
-}
-
-samplesz = -1
-trainable = tune.with_parameters(train_linevd, samplesz=samplesz)
-run_id = svd.get_run_id()
-savepath = svd.get_dir(svd.processed_dir() / f"raytune_{samplesz}" / run_id)
-
-analysis = tune.run(
-    trainable,
-    resources_per_trial={"cpu": 1, "gpu": 1},
-    metric="val_loss",
-    mode="min",
-    config=config,
-    num_samples=1000,
-    name="tune_linevd",
-    local_dir=savepath,
-    keep_checkpoints_num=2,
-    checkpoint_score_attr="min-val_loss",
-)
---
+++ sastvd/scripts/rq5.py
@@ -0,0 +1,44 @@
+import sastvd as svd
+import sastvd.linevd.run as lvdrun
+from ray import tune
+
+config = {
+    "hfeat": tune.choice([512]),
+    "embtype": tune.choice(["codebert"]),
+    "stmtweight": tune.choice([1, 2, 5, 8, 10]),
+    "hdropout": tune.choice([0.25, 0.3]),
+    "gatdropout": tune.choice([0.15, 0.2]),
+    "modeltype": tune.choice(["gat1layer"]),
+    "gnntype": tune.choice(["gat"]),
+    "loss": tune.choice(["ce"]),
+    "scea": tune.choice([0.4, 0.5, 0.6]),
+    "gtype": tune.choice(["pdg"]),
+    "batch_size": tune.choice([1024]),
+    "multitask": tune.choice(["linemethod"]),
+    "splits": tune.choice(
+        [
+            "crossproject-linux",
+            "crossproject-Chrome",
+            "crossproject-Android",
+            "crossproject-qemu",
+        ]
+    ),
+}
+
+samplesz = -1
+run_id = svd.get_run_id()
+sp = svd.get_dir(svd.processed_dir() / f"raytune_crossproject_{samplesz}" / run_id)
+trainable = tune.with_parameters(lvdrun.train_linevd, samplesz=samplesz, savepath=sp)
+
+analysis = tune.run(
+    trainable,
+    resources_per_trial={"cpu": 1, "gpu": 1},
+    metric="val_loss",
+    mode="min",
+    config=config,
+    num_samples=1000,
+    name="tune_linevd",
+    local_dir=sp,
+    keep_checkpoints_num=2,
+    checkpoint_score_attr="min-val_loss",
+)
