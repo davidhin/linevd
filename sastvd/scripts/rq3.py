791aa1da4afcca123c4abe201b26146589a4a70a
David Hin <hello@davidhin.com>
2021-09-10 13:02:14 +0930

    Add other RQ scripts

--- sastvd/scripts/rq3.py
+++ sastvd/scripts/rq3.py
@@ -4,23 +4,23 @@ from ray import tune
 
 config = {
     "hfeat": tune.choice([512]),
-    "embtype": tune.choice(["codebert", "glove", "doc2vec"]),
+    "embtype": tune.choice(["codebert"]),
     "stmtweight": tune.choice([1, 5, 10]),
     "hdropout": tune.choice([0.25, 0.3]),
     "gatdropout": tune.choice([0.15, 0.2]),
-    "modeltype": tune.choice(["mlponly"]),
+    "modeltype": tune.choice(["gat1layer"]),
     "gnntype": tune.choice(["gat"]),
     "loss": tune.choice(["ce"]),
     "scea": tune.choice([0.4, 0.5, 0.6]),
     "gtype": tune.choice(["pdg"]),
     "batch_size": tune.choice([1024]),
-    "multitask": tune.choice(["line"]),
+    "multitask": tune.choice(["line", "linemethod"]),
     "splits": tune.choice(["default"]),
 }
 
 samplesz = -1
 run_id = svd.get_run_id()
-sp = svd.get_dir(svd.processed_dir() / f"raytune_features_{samplesz}" / run_id)
+sp = svd.get_dir(svd.processed_dir() / f"raytune_method_{samplesz}" / run_id)
 trainable = tune.with_parameters(lvdrun.train_linevd, samplesz=samplesz, savepath=sp)
 
 analysis = tune.run(
