7b7212879b456e75a2f871d98c4c7fe47989c764
David Hin <hello@davidhin.com>
2021-09-14 15:46:57 +0930

    Update rqtest RAM

--- hpc/run_rqtest.sh
+++ hpc/run_rqtest.sh
@@ -3,7 +3,7 @@
 #SBATCH -N 1
 #SBATCH -n 2
 #SBATCH --time=48:00:00
-#SBATCH --mem=32GB
+#SBATCH --mem=64GB
 #SBATCH --gres=gpu:1
 #SBATCH --err="hpc/logs/rqT_%A.info"
 #SBATCH --output="hpc/logs/rqT_%A.info"
