10b3af740d0f1f16d2f3766664c57354b2f36ab9
David Hin <hello@davidhin.com>
2021-09-16 14:11:19 +0930

    Update testdfs

--- sastvd/linevd/test_dfs.py
+++ sastvd/linevd/test_dfs.py
@@ -2,54 +2,6 @@ from glob import glob
 
 import pandas as pd
 import sastvd as svd
-from scipy.stats import mannwhitneyu
-
-# %% RQ2
-results = glob(str(svd.outputs_dir() / "rq2/*.csv"))
-res_df = pd.concat([pd.read_csv(i) for i in results])
-res_df = res_df.sort_values("stmt_f1", ascending=0)
-metrics = [i for i in res_df.columns if "stmt" in i]
-res_df = res_df.groupby("trial_id").head(1)
-
-res_df[["stmt_f1"] + [i for i in res_df.columns if "config" in i]]
-
-res_df.groupby(["config/gnntype", "config/gtype"]).mean()[metrics]
-
-print(
-    res_df.groupby(["config/modeltype"])
-    .head(1)[["config/modeltype", "config/gnntype"] + metrics]
-    .to_latex(index=0)
-)
-print(
-    res_df.groupby(["config/gtype"])
-    .head(1)[["config/gtype"] + metrics]
-    .to_latex(index=0)
-)
-
-# %% RQ1
-results = glob(str(svd.outputs_dir() / "rq1/*.csv"))
-res_df = pd.concat([pd.read_csv(i) for i in results])
-res_df = res_df.sort_values("stmt_f1", ascending=0)
-metrics = [i for i in res_df.columns if "stmt" in i]
-res_df = res_df.groupby("trial_id").head(1)
-
-l1 = res_df[res_df["config/embtype"] == "glove"].stmt_prauc.tolist()
-l2 = res_df[res_df["config/embtype"] == "codebert"].stmt_prauc.tolist()
-
-print("Statistics=%.3f, p=%.3f" % mannwhitneyu(l1, l2))
-
-res_df.groupby("config/embtype").mean()
-
-# compare samples
-l1 = res_df[res_df["config/gnntype"] == "gcn"].stmt_f1.tolist()
-l2 = res_df[res_df["config/gnntype"] == "gat"].stmt_f1.tolist()
-
-l1 = res_df[res_df["config/gtype"].apply(lambda x: "cfgcdg" in x)].stmt_f1.tolist()
-l2 = res_df[res_df["config/gtype"].apply(lambda x: "pdg+raw" in x)].stmt_f1.tolist()
-
-print("Statistics=%.3f, p=%.3f" % mannwhitneyu(l1, l2))
-
-res_df.iloc[0]
 
 # %% Phoenix
 results = glob(str(svd.outputs_dir() / "phoenix/rq_results/*.csv"))
