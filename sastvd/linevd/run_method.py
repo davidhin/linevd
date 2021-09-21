e2b783e0471c403b93db361e4a98a97c5d137446
David Hin <hello@davidhin.com>
2021-09-22 08:57:44 +0930

    Update run method

--- sastvd/linevd/run_method.py
+++ sastvd/linevd/run_method.py
@@ -3,51 +3,79 @@ import os
 import pytorch_lightning as pl
 import sastvd as svd
 import sastvd.linevd as lvd
+from ray import tune
+from ray.tune.integration.pytorch_lightning import (
+    TuneReportCallback,
+    TuneReportCheckpointCallback,
+)
 
 os.environ["SLURM_JOB_NAME"] = "bash"
 
-run_id = svd.get_run_id()
+
+def train_ml(
+    config, savepath, samplesz=-1, max_epochs=130, num_gpus=1, checkpoint_dir=None
+):
+    """Wrap Pytorch Lightning to pass to RayTune."""
+    model = lvd.LitGNN(
+        methodlevel=True,
+        nsampling=False,
+        model="gat1layer",
+        embtype="codebert",
+        loss="ce",
+        hdropout=0.3,
+        gatdropout=0.2,
+        mlpdropout=0.2,
+        num_heads=4,
+        multitask="line",
+        stmtweight=1,
+        gnntype=config["gnntype"],
+        lr=1e-4,
+    )
+
+    # Load data
+    data = lvd.BigVulDatasetLineVDDataModule(
+        batch_size=32,
+        sample=samplesz,
+        methodlevel=True,
+        nsampling=False,
+        nsampling_hops=2,
+        gtype="pdg+raw",
+        splits="default",
+    )
+
+    # # Train model
+    checkpoint_callback = pl.callbacks.ModelCheckpoint(monitor="val_loss")
+    metrics = ["train_loss", "val_loss", "val_auroc"]
+    raytune_callback = TuneReportCallback(metrics, on="validation_end")
+    rtckpt_callback = TuneReportCheckpointCallback(metrics, on="validation_end")
+    trainer = pl.Trainer(
+        gpus=1,
+        auto_lr_find=False,
+        default_root_dir=savepath,
+        num_sanity_val_steps=3,
+        callbacks=[checkpoint_callback, raytune_callback, rtckpt_callback],
+        max_epochs=max_epochs,
+    )
+    trainer.fit(model, data)
+
+
+config = {
+    "gnntype": tune.choice(["gat", "gcn"]),
+}
 samplesz = -1
-savepath = svd.get_dir(svd.processed_dir() / f"methodlevel_{samplesz}" / run_id)
+run_id = svd.get_run_id()
+sp = svd.get_dir(svd.processed_dir() / f"raytune_methodlevel_{samplesz}" / run_id)
+trainable = tune.with_parameters(train_ml, samplesz=samplesz, savepath=sp)
 
-# Load model
-model = lvd.LitGNN(
-    methodlevel=True,
-    nsampling=False,
-    model="gat2layer",
-    embtype="codebert",
-    loss="ce",
-    hdropout=0.3,
-    gatdropout=0.2,
-    mlpdropout=0.2,
-    num_heads=4,
-    multitask="line",
-    stmtweight=1,
-    gnntype="gat",
-    lr=1e-4,
+analysis = tune.run(
+    trainable,
+    resources_per_trial={"cpu": 1, "gpu": 1},
+    metric="val_loss",
+    mode="min",
+    config=config,
+    num_samples=1000,
+    name="tune_linevd",
+    local_dir=sp,
+    keep_checkpoints_num=2,
+    checkpoint_score_attr="min-val_loss",
 )
-
-# Load data
-data = lvd.BigVulDatasetLineVDDataModule(
-    batch_size=32,
-    sample=samplesz,
-    methodlevel=True,
-    nsampling=False,
-    nsampling_hops=2,
-    gtype="pdg+raw",
-    splits="default",
-)
-
-# Train model
-checkpoint_callback = pl.callbacks.ModelCheckpoint(monitor="val_loss")
-metrics = ["train_loss", "val_loss", "val_auroc"]
-trainer = pl.Trainer(
-    gpus=1,
-    auto_lr_find=True,
-    default_root_dir=savepath,
-    num_sanity_val_steps=0,
-    callbacks=[checkpoint_callback],
-    max_epochs=1000,
-)
-trainer.tune(model, data)
-trainer.fit(model, data)
