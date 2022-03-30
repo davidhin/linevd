1fca96a1ae44efa3b2af645a01c50ca82add6ba3
David Hin <a1720858@adelaide.edu.au>
2022-03-30 13:07:00 +1030

    Update get_data.sh

--- get_data.sh
+++ get_data.sh
@@ -23,14 +23,8 @@ else
     echo "Already downloaded bigvul data"
 fi
 
-if [[ ! -f "MSR_data_cleaned_SAMPLE.csv" ]]; then
-    gdown https://drive.google.com/uc\?id\=1SmUfm1ibLjSg6QAA5-jK7u0RL-noLGIF
-else
-    echo "Already downloaded bigvul sample data"
-fi
-
 if [[ ! -d joern-cli ]]; then
-    gdown https://drive.google.com/uc\?id\=1LwZcKkHqFBWUBOLfoC8mB6oFesV5iGbX
+    wget https://github.com/joernio/joern/releases/download/v1.1.260/joern-cli.zip
     unzip joern-cli.zip
     rm joern-cli.zip
 else
