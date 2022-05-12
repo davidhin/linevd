9e21d774899b373110819db773fed85ef7722dd8
bstee615 <benjaminjsteenhoek@gmail.com>
2022-05-12 13:33:07 -0500

    Add test options for getgraphs

--- sastvd/scripts/getgraphs.py
+++ sastvd/scripts/getgraphs.py
@@ -4,8 +4,8 @@ import sys
 
 import numpy as np
 import sastvd as svd
-import sastvd.helpers.datasets as svdd
 import sastvd.helpers.dataset_sard as svds
+import sastvd.helpers.datasets as svdd
 import sastvd.helpers.joern as svdj
 import sastvd.helpers.sast as sast
 
@@ -22,6 +22,7 @@ def preprocess(row, run_sast=True):
 
     if row["dataset"] == "sard":
         savedir = svd.get_dir(svd.processed_dir() / row["dataset"] / "code")
+        # print(row)
 
         # Write C Files
         fpath = savedir / f"{row['index']}.c"
@@ -30,8 +31,8 @@ def preprocess(row, run_sast=True):
                 f.write(row["code"])
 
         # Run Joern on code
-        if not os.path.exists(f"{fpath}.edges.json"):
-            svdj.full_run_joern(fpath, verbose=3)
+        if not os.path.exists(f"{fpath}.edges.json") or args.test:
+            svdj.full_run_joern(fpath, verbose=args.verbose)
     if row["dataset"] == "bigvul":
         savedir_before = svd.get_dir(svd.processed_dir() / row["dataset"] / "before")
         savedir_after = svd.get_dir(svd.processed_dir() / row["dataset"] / "after")
@@ -46,20 +47,26 @@ def preprocess(row, run_sast=True):
                 f.write(row["after"])
 
         # Run Joern on "before" code
-        if not os.path.exists(f"{fpath1}.edges.json"):
-            svdj.full_run_joern(fpath1, verbose=3)
+        if not os.path.exists(f"{fpath1}.edges.json") or args.test:
+            svdj.full_run_joern(fpath1, verbose=args.verbose)
 
         # Run Joern on "after" code
-        if not os.path.exists(f"{fpath2}.edges.json") and len(row["diff"]) > 0:
-            svdj.full_run_joern(fpath2, verbose=3)
+        if (not os.path.exists(f"{fpath2}.edges.json") or args.test) and len(row["diff"]) > 0:
+            svdj.full_run_joern(fpath2, verbose=args.verbose)
 
         # Run SAST extraction
-        if run_sast:
+        if args.run_sast:
             fpath3 = savedir_before / f"{row['id']}.c.sast.pkl"
             if not os.path.exists(fpath3):
                 sast_before = sast.run_sast(row["before"])
                 with open(fpath3, "wb") as f:
                     pkl.dump(sast_before, f)
+
+
+def test_preprocess():
+    row = {}
+    result = preprocess(row)
+    print(f"{result}")
 
 
 if __name__ == "__main__":
@@ -68,8 +75,10 @@ if __name__ == "__main__":
     parser.add_argument("dataset", choices=["bigvul", "sard"])
     parser.add_argument("--job_array_number")
     parser.add_argument("--num_jobs", default=100, type=int)
-    parser.add_argument("--workers", default=8, type=int)
-    parser.add_argument("--run_sast", action="store_false")
+    parser.add_argument("--workers", default=1, type=int)
+    parser.add_argument("--run_sast", action="store_true")
+    parser.add_argument("--test", action="store_true")
+    parser.add_argument("--verbose", type=int, default=3)
     args = parser.parse_args()
 
     if args.dataset == "bigvul":
@@ -83,5 +92,11 @@ if __name__ == "__main__":
 
     # Read Data
     df = df.reset_index().iloc[::-1]
-    splits = np.array_split(df, NUM_JOBS)
-    svd.dfmp(splits[JOB_ARRAY_NUMBER], preprocess, ordr=False, workers=args.workers)
+    if args.test:
+        print("TEST - getting head() only")
+        df = df.head()
+        args.verbose = 4
+    else:
+        splits = np.array_split(df, NUM_JOBS)
+        df = splits[JOB_ARRAY_NUMBER]
+    svd.dfmp(df, preprocess, ordr=False, workers=args.workers)
