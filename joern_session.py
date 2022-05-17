648e4d899ad2cb8391ac7705b634fa2e1a55facb
Benjamin Steenhoek <benjaminjsteenhoek@gmail.com>
2022-05-17 11:39:10 -0500

    Handle exceptions which were causing Input/Output error

--- joern_session.py
+++ joern_session.py
@@ -1,39 +1,73 @@
 # %%
 import subprocess
+from pathlib import Path
+import time
+import logging
+import signal
 
 
 class JoernSession:
-    def __init__(self, worker_id: int=0):
-        self.proc = subprocess.Popen(["joern"], stdin=subprocess.PIPE, encoding="utf-8")
+    def __init__(self, worker_id: int=0, output_filename: str=None):
+        self.stdout_file = stdout = stderr = None
+        if output_filename is not None:
+            stdout = self.stdout_file = open(output_filename, "w")
+            stderr = subprocess.STDOUT
+        else:
+            stdout = subprocess.PIPE
+            stderr = subprocess.STDOUT
+        self.proc = subprocess.Popen(
+            ["joern", "--nocolors"],
+            stdout=stdout, stderr=stderr,
+            stdin=subprocess.PIPE, encoding="utf-8"
+            )
+        print("started process")
         if worker_id != 0:
-            self.switch_workspace(f"workers/{worker_id}")
+            workspace = f"workers/{worker_id}"
+            self.switch_workspace(workspace)
 
     def close(self):
         try:
-            outs, errs = self.proc.communicate("exit\n", timeout=15)
-        except subprocess.TimeoutExpired:
-            self.proc.kill()
-            outs, errs = self.proc.communicate(timeout=15)
+            print("close")
+            if self.stdout_file is not None:
+                self.stdout_file.close()
+        finally:
+            try:
+                outs, errs = self.proc.communicate("exit\nN\n", timeout=60)
+            except subprocess.TimeoutError:
+                self.proc.terminate()
+                self.proc.wait()
+    
+    def send_line(self, cmd: str):
+        print(f"send_line {cmd}")
+        self.proc.stdin.write(f"{cmd}\n")
 
     """
     Joern commands
     """
-    
-    def send_line(self, cmd: str):
-        self.proc.stdin.write(f"{cmd}\n")
+
+    def run_script(self, script: str, params, import_first=True):
+        if import_first:
+            scriptdir: Path = Path("storage/external")
+            scriptdir_str = str(scriptdir)
+            if scriptdir_str.endswith("/"):
+                scriptdir_str = scriptdir_str[:-1]
+            scriptdir_str = scriptdir_str.replace("/", ".")
+            self.send_line(f"""import $file.{scriptdir_str}.{script}""")
+
+        params_str = ", ".join(f'{k}="{v}"' for k, v in params.items())
+        return self.send_line(f"""{script}.exec({params_str})""")
 
     def switch_workspace(self, filepath: str):
-        return self.send_line(f"""switchWorkspace("{filepath}")\n""")
+        self.send_line(f"""switchWorkspace("{filepath}")""")
 
     def import_code(self, filepath: str):
-        return self.send_line(f"""importCode("{filepath}")\n""")
+        self.send_line(f"""importCode("{filepath}")""")
         
     def delete(self):
-        return self.send_line(f"delete\n")
+        self.send_line(f"delete")
 
     def list_workspace(self):
-        return self.send_line("workspace\n")
-
+        self.send_line("workspace")
 
 
 def test_interaction():
@@ -61,3 +95,13 @@ def test_worker():
         sess2.delete()
         sess1.close()
         sess2.close()
+
+
+def test_script():
+    sess = JoernSession()
+    try:
+        sess.import_code("x42/c/X42.c")
+        sess.run_script("get_dataflow_output", params={"filename": "x42/c/X42.c", "problem": "reachingdef"})
+        sess.delete()
+    finally:
+        sess.close()
